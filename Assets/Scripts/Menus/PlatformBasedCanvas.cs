8ee8a2473b88b4ca60229fbe103fdd0aee73ecd1
Victor Gheno <victorgheno@hotmail.com>
2021-12-20 10:49:22 -0300

    Updated HUD to match the target platform automatically

---
+++ Assets/Scripts/Menus/PlatformBasedCanvas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBasedCanvas : MonoBehaviour
+{
+    public GameObject[] mobileHUD;
+    public GameObject[] PCHUD;
+
+
+    private void Awake()
+    {
+#if UNITY_STANDALONE
+        foreach(GameObject o in mobileHUD)
+        {
+            o.SetActive(false);
+        }
+        foreach(GameObject o in PCHUD)
+        {
+            o.SetActive(true);
+        }
+
+#elif UNITY_ANDROID
+        foreach (GameObject o in mobileHUD)
+        {
+            o.SetActive(true);
+        }
+        foreach(GameObject o in PCHUD)
+        {
+            o.SetActive(false);
+        }
+
+#endif
+    }
+}
